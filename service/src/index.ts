9772f8cb37155a5e248c258206c03b22dcc78381
xiehui <xiehui@xk.design>
2023-04-19 00:42:15 +0800

    feat：signup/login for server

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,11 +1,28 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import Redis from 'ioredis'
 import type { RequestProps } from './types'
 import type { ChatMessage } from './chatgpt'
 import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
 import { auth } from './middleware/auth'
 import { limiter } from './middleware/limiter'
 import { isNotEmptyString } from './utils/is'
-const connection = require('../src/models/database')
+// const redis = require('redis')
+const { executeQuery } = require('./models/database')
+const client = new Redis({
+  host: 'localhost', // Redis server address
+  port: 6379, // Redis server port number
+  // password: 'your_redis_password', // Redis server password
+})
+
+client.on('connect', () => {
+  console.log('Redis client connected')
+})
+
+client.on('error', (err) => {
+  console.error('Redis client error:', err)
+})
 
 const app = express()
 const router = express.Router()
@@ -20,18 +37,101 @@ app.all('*', (_, res, next) => {
   next()
 })
 
+const privateKey = 'your_private_key_here'
+
+// 注册接口
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password, invitationCode } = req.body
+    // 验证 email 和 password 是否合法
+    if (!email || !password)
+      throw new Error('Invalid email or password')
+
+    // Check invitation code first
+    if (invitationCode) {
+      const [invitation] = await executeQuery(
+        'SELECT * FROM invitation_codes WHERE code = ?',
+        [invitationCode],
+      )
+      if (!invitation)
+        throw new Error('Invalid invitation code')
+    }
+    else {
+      throw new Error('Invitation code is required')
+    }
+
+    // 检查数据库中是否已经存在该用户
+    const [user] = await executeQuery(
+      'SELECT * FROM users WHERE email = ?',
+      [email],
+    )
+    if (user)
+      throw new Error('User already exists')
+
+    // ... (rest of the code)
+  }
+  catch (error) {
+    res.send({ status: 'fail', message: error.message })
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    console.log(req.body)
+    // 验证 email 和 password 是否合法
+    if (!email || !password)
+      throw new Error('Invalid email or password')
+
+    // 检查数据库中是否存在该用户
+    const [user] = await executeQuery(
+      'SELECT * FROM users WHERE email = ?',
+      [email],
+    )
+    console.log(user)
+    if (!user)
+      throw new Error('User not found')
+
+    // 使用 bcrypt 验证密码是否匹配
+    const match = await bcrypt.compare(password, user.password)
+    if (!match)
+      throw new Error('Invalid password')
+
+    // 检查 Redis 中是否存在该用户的 token
+    client.get(email, async (err, reply) => {
+      if (err)
+        throw new Error(err)
+
+      if (reply) {
+        console.log(`Token exists for user ${email}: ${reply}`)
+        res.cookie('token', reply, { maxAge: 3600 * 1000 })
+        res.send({ status: 'success', token: reply })
+      }
+      else {
+        // 生成一个新的 token 并将其存储到 Redis 中
+        const token = jwt.sign({ email }, privateKey)
+        await client.set(email, token, 'EX', 3600, (err, reply) => {
+          if (err)
+            throw new Error(err)
+          console.log(`Token ${token} is set for user ${email}`)
+        })
+        // 设置 cookie，有效期为 1 小时
+        res.cookie('token', token, { maxAge: 3600 * 1000 })
+        res.send({ status: 'success', token })
+      }
+    })
+  }
+  catch (error) {
+    res.send({ status: 'fail', message: error.message })
+  }
+})
+
 router.post('/chat-process', [auth, limiter], async (req, res) => {
   res.setHeader('Content-type', 'application/octet-stream')
 
   try {
     const { prompt, options = {}, systemMessage } = req.body as RequestProps
     let firstChunk = true
-
-    // // 执行SQL查询
-    // const results = await connection.promise().query('SELECT * FROM users')
-		//
-    // // 处理查询结果
-    // console.log('Query results: ', results[0])
 
     await chatReplyProcess({
       message: prompt,
@@ -66,22 +166,6 @@ router.post('/session', async (req, res) => {
     const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
     const hasAuth = isNotEmptyString(AUTH_SECRET_KEY)
     res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
-  }
-  catch (error) {
-    res.send({ status: 'Fail', message: error.message, data: null })
-  }
-})
-
-router.post('/verify', async (req, res) => {
-  try {
-    const { token } = req.body as { token: string }
-    if (!token)
-      throw new Error('Secret key is empty')
-
-    if (process.env.AUTH_SECRET_KEY !== token)
-      throw new Error('密钥无效 | Secret key is invalid')
-
-    res.send({ status: 'Success', message: 'Verify successfully', data: null })
   }
   catch (error) {
     res.send({ status: 'Fail', message: error.message, data: null })
--- service/src/models/database.js
+++ service/src/models/database.js
@@ -1,33 +1,26 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'qqq111',
   database: 'mydatabase',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 })
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database: ', err)
-    return
+async function executeQuery(query, values) {
+  const connection = await pool.getConnection()
+  try {
+    const [rows] = await connection.query(query, values)
+    return rows
   }
-})
+  finally {
+    connection.release()
+  }
+}
 
-// 执行 SQL 查询
-connection.query('SELECT * FROM users', (err, results) => {
-  if (err) {
-    console.error('Error executing query: ', err)
-    return
-  }
-  console.log('Query results: ', results)
-})
-
-// 关闭数据库连接
-connection.end((err) => {
-  if (err) {
-    console.error('Error closing database connection: ', err)
-    return
-  }
-  console.log('Database connection closed')
-})
+module.exports = {
+  executeQuery,
+}
