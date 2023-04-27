2d16eaa296110fc4e7bc052bc0a745a2be41bf70
xiehui <xiehui@xk.design>
2023-04-27 23:46:38 +0800

    fix: bugs#setting

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -2,7 +2,8 @@
 import { computed, ref } from 'vue'
 import { NModal, NTabPane, NTabs } from 'naive-ui'
 import General from './General.vue'
-import { useAuthStore } from '@/store'
+import Advanced from './Advanced.vue'
+// import { useAuthStore } from '@/store'
 import { SvgIcon } from '@/components/common'
 
 interface Props {
@@ -17,9 +18,9 @@ const props = defineProps<Props>()
 
 const emit = defineEmits<Emit>()
 
-const authStore = useAuthStore()
+// const authStore = useAuthStore()
 
-const isChatGPTAPI = computed<boolean>(() => !!authStore.isChatGPTAPI)
+// const isChatGPTAPI = computed<boolean>(() => !!authStore.isChatGPTAPI)
 
 const active = ref('General')
 
@@ -37,7 +38,7 @@ const show = computed({
   <NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 95%; max-width: 640px">
     <div>
       <NTabs v-model:value="active" type="line" animated>
-        <NTabPane name="General" tab="General" v-show="isChatGPTAPI">
+        <NTabPane name="General" tab="General">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:file-user-line" />
             <span class="ml-2">{{ $t('setting.general') }}</span>
@@ -46,15 +47,15 @@ const show = computed({
             <General />
           </div>
         </NTabPane>
-        <!--        <NTabPane v-if="isChatGPTAPI" name="Advanced" tab="Advanced"> -->
-        <!--          <template #tab> -->
-        <!--            <SvgIcon class="text-lg" icon="ri:equalizer-line" /> -->
-        <!--            <span class="ml-2">{{ $t('setting.advanced') }}</span> -->
-        <!--          </template> -->
-        <!--          <div class="min-h-[100px]"> -->
-        <!--            <Advanced /> -->
-        <!--          </div> -->
-        <!--        </NTabPane> -->
+        <NTabPane v-if="true" name="Advanced" tab="Advanced">
+          <template #tab>
+            <SvgIcon class="text-lg" icon="ri:equalizer-line" />
+            <span class="ml-2">{{ $t('setting.advanced') }}</span>
+          </template>
+          <div class="min-h-[100px]">
+            <Advanced />
+          </div>
+        </NTabPane>
         <!--        <NTabPane name="Config" tab="Config"> -->
         <!--          <template #tab> -->
         <!--            <SvgIcon class="text-lg" icon="ri:list-settings-line" /> -->
