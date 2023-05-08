171a3749dd78f638859ade39a3a0c2f529561933
xiehui <xiehui@xk.design>
2023-05-09 00:30:30 +0800

    feat: Login.vue

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { computed, reactive, ref } from 'vue'
-import { NButton, NInput, useNotification } from 'naive-ui'
+import type { FormInst, FormItemInst, FormItemRule, FormRules } from 'naive-ui'
+import { NButton, NForm, NFormItem, NInput, useNotification } from 'naive-ui'
 import axios from '@/utils/request/axios'
 const notification = useNotification()
 const loginForm = reactive({
@@ -86,61 +87,141 @@ function handleLogin() {
   }
   submitLogin()
 }
+
+interface ModelType {
+  age: string | null
+  password: string | null
+  reenteredPassword: string | null
+}
+const formRef = ref<FormInst | null>(null)
+const rPasswordFormItemRef = ref<FormItemInst | null>(null)
+const modelRef = ref<ModelType>({
+  age: null,
+  password: null,
+  reenteredPassword: null,
+})
+const rules: FormRules = {
+  email: [
+    {
+      required: true,
+      validator(rule: FormItemRule, value: string) {
+        if (!value)
+          return new Error('需要邮箱')
+        else if (!/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value))
+          return new Error('请输入有效的邮箱地址')
+
+        return true
+      },
+      trigger: ['input', 'blur'],
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: ['input', 'blur'],
+    },
+    {
+      validator(rule: FormItemRule, value: string) {
+        if (value.length < 6)
+          return new Error('密码至少需要6位')
+				 else if (!(/[a-zA-Z]/.test(value) && /\d/.test(value)))
+          return new Error('密码必须包含英文和数字的组合')
+
+        return true
+      },
+      trigger: ['input', 'blur'],
+    },
+  ],
+  invitationCode: [
+    {
+      validator(rule: FormItemRule, value: string) {
+        if (value && value.length !== 6)
+          return new Error('邀请码需要为6位数字')
+				 else if (value && !/^\d{6}$/.test(value))
+          return new Error('邀请码必须是6位数字')
+
+        return true
+      },
+      trigger: ['input', 'blur'],
+    },
+  ],
+
+}
+function handlePasswordInput() {
+  if (modelRef.value.reenteredPassword)
+    rPasswordFormItemRef.value?.validate({ trigger: 'password-input' })
+}
 </script>
 
 <template>
   <div class="bgc">
+    <div class="login-bgc">
+      <div class="play-gpt" />
+      <div class="bgc-container" />
+    </div>
     <div class="login-container">
-      <div class="title">
-        OpenAI-chatgpt
+			<div class="small-bgc"></div>
+      <div class="logo">
+        <div class="logo-pic" />
+        <div class="logo-word" />
       </div>
+			<div class="small-logo">
+				<div class="logo-pic" />
+				<div class="logo-word" >登录到ChatGPT Bolt</div>
+			</div>
+
       <div class="input-group">
-        <label>邮箱：</label>
-        <NInput v-model:value="loginForm.email" type="text" style="width: 200px" />
+        <NForm ref="formRef" :model="loginForm" :rules="rules">
+          <NFormItem path="email" label="邮箱">
+            <NInput v-model:value="loginForm.email" size="large" @keydown.enter.prevent />
+          </NFormItem>
+          <NFormItem path="password" label="密码">
+            <NInput
+              v-model:value="loginForm.password"
+              type="password"
+              show-password-on="mousedown"
+              size="large"
+              @input="handlePasswordInput"
+              @keydown.enter.prevent
+            />
+          </NFormItem>
+          <NFormItem v-if="!loginStatus" path="invitationCode" label="邀请码">
+            <NInput
+              v-model:value="loginForm.invitationCode"
+							placeholder="邀请码（可选）"
+              size="large"
+              @keydown.enter.prevent
+            />
+          </NFormItem>
+        </NForm>
       </div>
-      <div class="input-group">
-        <label>密码：</label>
-        <NInput
-          v-model:value="loginForm.password"
-          type="password"
-          show-password-on="mousedown"
-          placeholder="密码"
-          :maxlength="8"
-          style="width: 200px"
-        />
-      </div>
-      <div v-show="!loginStatus" class="input-group">
-        <label>邀请码：</label>
-        <NInput
-          v-model:value="loginForm.invitationCode"
-          show-password-on="mousedown"
-          placeholder="非必填"
-          :minlength="6"
-          :maxlength="12"
-          style="width: 200px"
-        />
-      </div>
+
       <div class="btn">
         <NButton
-          v-if="loginStatus" type="success" :disabled="!isReadyLogin"
-          @click="handleLogin"
+          v-if="loginStatus" :disabled="!isReadyLogin"
+          class="submit" @click="handleLogin"
         >
           登录
         </NButton>
         <NButton
           v-else
-          type="success"
+          class="submit"
           :disabled="!isReadyLogin"
           @click="handleLogin"
         >
           注册
         </NButton>
-        <NButton v-if="loginStatus" style="margin-left: auto;color: white" @click="loginStatus = false">
-          我要去注册
-        </NButton>
-        <NButton v-else style="margin-left: auto;color: white" @click="loginStatus = true">
-          我要去登录
-        </NButton>
+
+        <div v-if="loginStatus" class="register">
+          <span class="no-account">还没有账号？</span>
+          <span class="go-to-r" @click="loginStatus = false">马上注册</span>
+        </div>
+
+        <div v-if="!loginStatus" class="register">
+          <span class="no-account">已经有账号？</span>
+          <span class="go-to-r" @click="loginStatus = true">马上登录</span>
+        </div>
       </div>
     </div>
   </div>
@@ -148,69 +229,154 @@ function handleLogin() {
 
 <style scoped lang="less">
 .bgc{
-	//background: no-repeat center center /100% url("../../assets/bgc.png");
-	//background-size: cover;
+	display: flex;
 	background: white;
 	opacity: 1;
 	width: 100%;
 	height: 100%;
 }
+.login-bgc{
+	position: relative;
+	width: 683px;
+	.play-gpt{
+		position: absolute;
+		bottom: 25%;
+		left: 50%;
+		margin-left: -163.25px;
+		margin-top: -30px;
+		width: 326.49px;
+		height: 65.5px;
+		background: no-repeat center center /100% url("../../assets/playgpt.png");
+	}
+	.bgc-container{
+		position: absolute;
+		top: 45%;
+		left: 50%;
+		margin-top: -210px;
+		margin-left: -341.5px;
+		min-width: 683px;
+		height: 420px;
+		background: no-repeat center center /100% url("../../assets/bg-pic.png");
+	}
+	width: 70vw;
+	height: 100%;
+	background: linear-gradient(246deg, #D7ECFF 0%, rgba(255,255,255,0.94) 100%);
+	opacity: 1;
+}
 .login-container {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	position: absolute;
-	top: 0;
-	right: 0;
 	width: 30vw;
 	min-width: 335px;
 	height: 100%;
-	background-color: #4b9e5f;
-.title{
-	color: white;
-	font-size: 22px;
-	margin-bottom: 25px;
-}
-}
+	overflow: hidden;
+	background-color: #fff;
+	.logo{
+		margin-top: 156px;
+		.logo-pic{
+			width: 120px;
+			height: 120px;
+			margin: 0 auto;
+			background: no-repeat center center /100% url("../../assets/logo.png");
+		}
+		.logo-word{
+			margin: 0 auto;
+			margin-top: 14px;
+			width: 250px;
+			height: 48px;
+			background: no-repeat center center /100% url("../../assets/logo-word.png");
+		}
+	}
+	.small-logo{
+		display: none;
+	}
+	.input-group{
+		width: 70%;
+		margin: 0 auto;
+		margin-top: 49px;
+		NInput{
+			height: 60px;
+		}
+	}
 
-.input-group {
-	margin-bottom: 1rem; display: flex;height: 40px;line-height: 40px;
-label{color: white}
-}
-.btn{
-	width: 50%;
-	display: flex;
-	align-content: space-between;
-	margin-top: 20px;
-}
-
-#input{
-	width: 200px;
-}
-
-label {
-display: inline-block;width: 80px;
-}
-
-button {cursor: pointer;}
-
-@media (max-width: 500px) {
-	.bgc {
-		//background: no-repeat center/100% url("../../assets/bg_mobile.png");
-		.login-container {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			justify-content: center;
+	.btn{
+		width: 70%;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		margin-top: 10px;
+		.submit{
 			width: 100%;
-			height: 100%;
-			background-color: #4b9e5f;
-			.title{
+			background: linear-gradient(43deg, #51A2FF 0%, #3D86FF 100%);
+			box-shadow: 0px 4px 4px 0px rgba(67,142,255,0.25);
+			color: #FEFEFE;;
+			&:hover{
 				color: white;
-				font-size: 22px;
-				margin-bottom: 25px;
 			}
+		}
+		.register{
+			margin-top: 22px;
+			height: 26px;
+			width: 100%;
+			text-align: center;
+			.no-account{
+				width: 185px;
+				height: 26px;
+				font-weight: 400;
+				color: #737373;
+				line-height: 25px;
+			}
+			.go-to-r{
+				cursor: pointer;
+				color: rgb(66, 173, 255);
+			}
+		}
+	}
+}
+
+@media (max-width: 1024px) {
+	.login-bgc{
+		width: 0px;
+		overflow: hidden;
+}
+	.login-container{
+		position: relative;
+		width: 100%;
+		.small-bgc{
+			position: absolute;
+			top: 20px;
+			right: 20px;
+			background: no-repeat center center /100% url("../../assets/small-bgc.png");
+			width: 40vw;
+			height: 125px;
+		}
+	}
+
+	.logo{
+		display: none;
+	}
+
+	.small-logo{
+		display: block!important;
+		width: 70%;
+		margin: 0 auto;
+		margin-top: 156px;
+		height: 200px;
+		.logo-pic{
+			margin: 0 auto;
+			width: 120px;
+			height: 120px;
+			background: no-repeat center center /100% url("../../assets/small-logo.png");
+		}
+		.logo-word{
+			margin: 0 auto;
+			margin-top: 14px;
+			height: 40px;
+			text-align: center;
+			font-size: 26px;
+			font-weight: 400;
+			line-height: 40px;
+			background: linear-gradient(180deg, #6EBBFF 0%, #0A64FF 100%);
+			-webkit-background-clip: text;
+			-webkit-text-fill-color: transparent;
 		}
 	}
 }
